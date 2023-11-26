a12c8e26b0130af87604d94f93a3accedbf8acda
snele45 <sneskovic4@hotmail.com>
2023-11-27 00:12:05 +0100

    implemented knn algorithm and added one more face

--- FaceDataCollect.py
+++ FaceDataCollect.py
@@ -1,5 +1,6 @@
 import cv2
 import numpy as np
+
 cam = cv2.VideoCapture(0)
 
 fileName = input("Enter the name of the person : ")
@@ -9,40 +10,43 @@ model = cv2.CascadeClassifier("haarcascade_frontalface_alt.xml")
 
 faceData = []
 count = 0
+cropped_face = None  # Initialize the variable outside the loop
 
 while True:
-    success,img = cam.read()
+    success, img = cam.read()
     if not success:
         print("Camera read failed!")
 
-    
-    faces = model.detectMultiScale(img,1.3,5)
+    faces = model.detectMultiScale(img, 1.3, 5)
 
-    faces = sorted(faces,key=lambda f:f[2]*f[3])
-    if len(faces)>0:
+    faces = sorted(faces, key=lambda f: f[2] * f[3])
+    if len(faces) > 0:
         f = faces[-1]
-        x,y,w,h = f
-        cv2.rectangle(img,(x,y),(x+w,y+h),(0,255,0),2)
+        x, y, w, h = f
+        cv2.rectangle(img, (x, y), (x + w, y + h), (0, 255, 0), 2)
 
-        cropped_face = img[y-offset:y+h+offset,x-offset:x+w+offset]
-        cropped_face = cv2.resize(cropped_face,(100,100))
-        count+=1
-        if count %10 == 0:
+        cropped_face = img[y - offset : y + h + offset, x - offset : x + w + offset]
+        cropped_face = cv2.resize(cropped_face, (100, 100))
+        count += 1
+        if count % 10 == 0:
             faceData.append(cropped_face)
-            print("Saved so far "+ str(len(faceData)))
+            print("Saved so far " + str(len(faceData)))
 
     cv2.imshow("Image window", img)
-    cv2.imshow("Cropped Face",cropped_face)
+    if cropped_face is not None:
+        cv2.imshow("Cropped Face", cropped_face)
+
     key = cv2.waitKey(1)
-    if key == ord('q'):
+    if key == ord("q"):
         break
+
 faceData = np.asarray(faceData)
 m = faceData.shape[0]
-faceData = faceData.reshape((m,-1))
+faceData = faceData.reshape((m, -1))
 
 print(faceData.shape)
 file = dataset_path + fileName + ".npy"
-np.save(file,faceData)
+np.save(file, faceData)
 print("Data saved!")
 
 cam.release()
--- FaceRecognise.py
+++ FaceRecognise.py
@@ -7,7 +7,7 @@ classId = 0
 faceData = []
 labels = []
 nameMap = {}
-
+offset = 40
 for f in os.listdir(dataset_path):
     if f.endswith(".npy"):
 
@@ -25,10 +25,69 @@ for f in os.listdir(dataset_path):
 
 
 #merge inputs
-X = np.concatenate(faceData,axis= 0)
+X_train = np.concatenate(faceData,axis= 0)
 #merge targets
-y = np.concatenate(labels,axis=0).reshape((-1,1)) #one column vector
+y_train = np.concatenate(labels,axis=0).reshape((-1,1)) #one column vector
 
-print(X.shape)
-print(y.shape)
+print(X_train.shape)
+print(y_train.shape)
 print(nameMap)
+
+#KNN algorithm
+
+def dist(p,q):
+    return np.sqrt(np.sum((p-q)**2))
+
+def knn(X,y,xt,k=5):
+    m = X.shape[0]
+    dlist = []
+
+    for i in range(m):
+        d = dist(X[i],xt)
+        dlist.append((d,y[i]))
+
+    dlist = sorted(dlist)
+    dlist = np.array(dlist[:k])
+    labels = dlist[:,1]
+
+    labels, cnts = np.unique(labels, return_counts=True)
+    idx = cnts.argmax()
+    pred = labels[idx]
+
+    return int(pred)
+
+#predictions
+
+cam = cv2.VideoCapture(0)
+
+model = cv2.CascadeClassifier("haarcascade_frontalface_alt.xml")
+while True:
+    success,img = cam.read()
+    if not success:
+        print("Camera read failed!")
+
+    
+    faces = model.detectMultiScale(img,1.3,5)
+
+    for f in faces:
+        x,y,w,h = f
+        cv2.rectangle(img,(x,y),(x+w,y+h),(0,255,0),2)
+
+        cropped_face = img[y-offset:y+h+offset,x-offset:x+w+offset]
+        cropped_face = cv2.resize(cropped_face,(100,100))
+        
+        
+        #predict the name using  KNN ALGORITHM
+        predictedClass = knn(X_train,y_train,cropped_face.flatten()) #flatten because shape (100,100,3) and (100,100)
+
+        predictedName = nameMap[predictedClass]
+        cv2.putText(img,predictedName,(x,y-20),cv2.FONT_HERSHEY_SIMPLEX,1,(255,0,0),2,cv2.LINE_AA)
+        cv2.rectangle(img,(x,y),(x+w,y+h),(0,255,0),2)
+
+    cv2.imshow("Prediction window",img)
+
+    key = cv2.waitKey(1)
+    if key == ord('q'):
+        break
+cam.release()
+cv2.destroyAllWindows()
